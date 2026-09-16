ab366f7daabab10c36a0631d5f1993a2eb19cbd8
OmidReza <omidreza.hjt@gmail.com>
2026-09-17 02:55:47 +0330

    setup and edit files for prometheus

--- project2/backend/app.py
+++ project2/backend/app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter(
+    "http_requests_total",
+    "Total number of HTTP requests"
+)
+
+@app.before_request
+def count_request():
+    REQUEST_COUNT.inc()
 
 @app.route("/")
 def home():
@@ -9,5 +19,12 @@ def home():
 @app.route("/health")
 def health():
     return "OK"
+
+@app.route("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        mimetype="text/plain"
+    )
 
 app.run(host="0.0.0.0", port=5000)
